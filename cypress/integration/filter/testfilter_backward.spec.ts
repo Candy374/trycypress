e07a9d9cb13aaa7f39d248d99f82cee845cdc042
huangling <lingling374@qq.com>
2021-08-04 18:21:35 +0800

    feat: add case forward property

--- cypress/integration/filter/testfilter_backward.spec.ts
+++ cypress/integration/filter/testfilter_backward.spec.ts
@@ -26,24 +26,69 @@ describe("The Filter level2", () => {
     cy.contains("查 询").click();
     cy.contains("杰哥").should("be.visible");
   });
+
+  it("查找 我的爱车  默认选中不为空", () => {
+    cy.get(`[data-ta-type="field"]`).click();
+
+    selectField(["属性字段", "我的爱车"]);
+    cy.get('[data-ta-key="notEmpty"]').should("be.visible");
+    cy.get('[data-ta-key="notEmpty"]').should("be.visible");
+    cy.contains("特斯拉").should("be.visible");
+    cy.contains("查 询").click();
+    cy.contains("杰哥").should("be.visible");
+    cy.contains("龙哥").should("be.visible");
+    cy.contains("曹立").should("be.visible");
+
+    selectOperator("为空");
+    cy.contains("查 询").click();
+    cy.contains("曹立").should("be.visible");
+  });
+
+  it("查找 我的爱车  默认选中不为空", () => {
+    cy.get(`[data-ta-type="field"]`).click();
+
+    selectField(["属性字段", "我的爱车"]);
+    selectConstraint("属性字段");
+
+    cy.get('[data-ta-key="属性字段"]').find(`[data-ta-type="field"]`).click();
+    selectField(["名称"]);
+    selectOperator("不包含", cy.get('[data-ta-key="属性字段"]'));
+    cy.get('[data-ta-key="属性字段"]')
+      .find('[data-ta-type="text"]')
+      .type("平价版");
+    cy.contains("查 询").click();
+    cy.contains("龙哥").should("be.visible");
+    cy.contains("杰哥").should("not.exist");
+  });
 });
 
 function selectField(values: string[]) {
-  const cascader = cy.get(".ant-cascader-menus.data-ta-type_field");
+  const cascader = cy.get(
+    ".ant-cascader-menus:not(.ant-cascader-menus-hidden)"
+  );
 
   for (const value of values) {
-    cy.get(".ant-cascader-menus.data-ta-type_field")
+    cy.get(".ant-cascader-menus:not(.ant-cascader-menus-hidden)")
       .contains(value)
       .should("be.visible");
-    cy.get(".ant-cascader-menus.data-ta-type_field").contains(value).click();
+    cy.get(".ant-cascader-menus:not(.ant-cascader-menus-hidden)")
+      .contains(value)
+      .click();
   }
 
   cascader.should("not.be.visible");
 }
 
-function selectOperator(operator) {
-  cy.get(`[data-ta-type="operator"]`).click();
+function selectOperator(operator, parent: any = cy) {
+  parent.find(`[data-ta-type="operator"]`).click();
   cy.get(".ant-select-dropdown:not(.ant-select-dropdown-hidden)")
     .contains(operator)
+    .click();
+}
+
+function selectConstraint(constraint) {
+  cy.contains("添加限制条件").click();
+  cy.get(".ant-dropdown:not(.ant-dropdown-hidden)")
+    .contains(constraint)
     .click();
 }
